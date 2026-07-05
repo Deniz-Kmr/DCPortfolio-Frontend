2e3625e2790a3e61904edf4a5179f93f900351d9
Deniz Çelik <https://github.com/Deniz-Kmr>
2026-07-05 14:59:45 +0300

    feat: add admin panel type mappings

---
+++ src/types/admin/adminEnumTypes.ts
@@ -0,0 +1,5 @@
+export type TodoStatus = 'Todo' | 'InProgress' | 'Done' | 'Cancelled';
+
+export type TodoPriority = 'Low' | 'Medium' | 'High';
+
+export type RoadmapStatus = 'Planned' | 'InProgress' | 'Completed' | 'Paused';
